f639035c13388bb8e2ce2db963668474a5e4de68
ViachaslauBY <70838254+ViachaslauBY@users.noreply.github.com>
2022-11-21 19:57:40 +0300

    fix swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  spaceBetween: 10,
+  loop: true,
 
+  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-
-  spaceBetween: 10,
-
-  loop: true,
+  }, 
 });
